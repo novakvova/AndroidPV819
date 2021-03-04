f1d65b548378b40aa74203fb8149a9b9a670b987
novakvova <novakvova@gmail.com>
2021-03-04 21:08:42 +0200

    upload image is good

--- app/src/main/java/com/example/testapp/LoginActivity.java
+++ app/src/main/java/com/example/testapp/LoginActivity.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.testapp.network.ImageRequester;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageRequester imageRequester;
+    private NetworkImageView myImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        String url = "https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/dogs_1280p_0.jpg?itok=6jQzdNB8";
+        String url = "https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg";
 
+        imageRequester = ImageRequester.getInstance();
+        myImage = findViewById(R.id.myimg);
+        imageRequester.setImageFromUrl(myImage, url);
 
     }
 
