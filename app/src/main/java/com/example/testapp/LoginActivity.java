5a0329b4532221b10b98fea51cf661667e27a24f
novakvova <novakvova@gmail.com>
2021-03-04 20:33:12 +0200

    Add volley lib

--- app/src/main/java/com/example/testapp/LoginActivity.java
+++ app/src/main/java/com/example/testapp/LoginActivity.java
@@ -12,6 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        String url = "https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/dogs_1280p_0.jpg?itok=6jQzdNB8";
 
 
     }
