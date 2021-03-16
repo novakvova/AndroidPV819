ee8ef5f1a658236ed179a6a6bd07c39ed8284547
radyvoniukkate <radyvoniukkate@gmail.com>
2021-03-16 18:46:06 +0200

    Add constants

--- TestApp/app/src/main/java/com/example/testapp/LoginActivity.java
+++ TestApp/app/src/main/java/com/example/testapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.android.volley.toolbox.NetworkImageView;
+import com.example.testapp.constants.Urls;
 import com.example.testapp.network.ImageRequester;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //String url = "https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg";
-        String url = "http://10.0.2.2:6937/images/lock.png";
+        String url = Urls.BASE+ "/images/lock.png";
 
         imageRequester = ImageRequester.getInstance();
         myImage = findViewById(R.id.myimg);
---
+++ TestApp/app/src/main/java/com/example/testapp/constants/Urls.java
@@ -0,0 +1,5 @@
+package com.example.testapp.constants;
+
+public class Urls {
+    public static final String BASE = "http://10.0.2.2:6937";
+}
