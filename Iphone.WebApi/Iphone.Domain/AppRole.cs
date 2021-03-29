9ac3f2990f6c8b14993fb7f86c1888c668502204
novakvova <novakvova@gmail.com>
2021-03-29 18:38:35 +0300

    Add EFDataContext

---
+++ Iphone.WebApi/Iphone.Domain/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iphone.Domain
+{
+    public class AppRole : IdentityRole<long>
+    {
+        public virtual ICollection<AppUserRole> UserRoles { get; set; }
+    }
+
+}
---
+++ Iphone.WebApi/Iphone.EFData/EFDataContext.cs
@@ -0,0 +1,42 @@
+﻿using Iphone.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iphone.EFData
+{
+    public class EFDataContext : IdentityDbContext<AppUser, AppRole, long, IdentityUserClaim<long>,
+                    AppUserRole, IdentityUserLogin<long>,
+                    IdentityRoleClaim<long>, IdentityUserToken<long>>
+    {
+        public EFDataContext(DbContextOptions<EFDataContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+        }
+    }
+}
