b867a5790fe56f23172ce301465da1856b1145fc
novakvova <novakvova@gmail.com>
2021-03-29 20:16:25 +0300

    Add JwtGenaration

---
+++ Iphone.WebApi/IPhone.Application/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Iphone.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPhone.Application.Interfaces
+{
+    public interface IJwtGenerator
+    {
+        string CreateToken(AppUser user);
+    }
+}
