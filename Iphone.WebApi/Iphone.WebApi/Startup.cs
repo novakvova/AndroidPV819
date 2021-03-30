9f7d9eb3873fa85bb79d8e3fea7f11da914d42a0
novakvova <novakvova@gmail.com>
2021-03-30 18:25:26 +0300

    Add Swagger auth

--- Iphone.WebApi/Iphone.WebApi/Startup.cs
+++ Iphone.WebApi/Iphone.WebApi/Startup.cs
@@ -93,7 +93,26 @@ namespace Iphone.WebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Iphone.WebApi", Version = "v1" });
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer"
+                    });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                    {
+                        new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference{
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },new List<string>()
+                    }
+                });
             });
+
+
 
 
         }
